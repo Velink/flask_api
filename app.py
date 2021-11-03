237772ce6dc0bf5c17816b5cbe8067b7d7e90fb3
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-11-03 15:50:59 +0000

    get route added

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from werkzeug import exceptions
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def welcome():
+    return 'Welcome to the Video Game API!'
+
+
+@app.route('/video-games', methods=['GET', 'POST'])
+def vg_handler():
+    if request.method == 'GET':
+        return jsonify([
+            {"name": "League of Legends", "ROI": 2009},
+            {"name": "Tomb Raider", "ROI": 2013},
+            {"name": "Skyrim", "ROI": 2011}
+        ]), 200
+    elif request.method == 'POST':
+        data = request.json
+        return f"You created a cat! The cat is called {data['name']}", 200
+
+
+@app.route('/cats/<int:cat_id>')
+def cat_handler(cat_id):
+    pass
+
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return jsonify({"message": f"Oops...{err}"}), 404
+
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_500(err):
+    return jsonify({"message": f"it's not you, it's us"}), 500
+
+
+# Boilerplate code which protects users from accidentally invoking the script when they don't want to
+if __name__ == '__main__':
+    app.run(debug=True)
